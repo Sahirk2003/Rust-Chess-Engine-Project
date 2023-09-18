9a875b81323b990b92b6bb32622bd0a252083bea
Sahir <sahirk2003@gmail.com>
2023-09-18 09:29:07 -0400

    added game state trackers to board structure

--- src/board.rs
+++ src/board.rs
@@ -11,4 +11,23 @@ struct Board {
     black_bishops: u64,
     black_queen: u64,
     black_king: u64,
+
+    // Game State Trackers
+    side_to_move: Color, // Tracking which side's turn it is
+    en_passant_target: Option <u8>, // Tracking squares where en passant is possible
+    castling_rights: CastlingRights, // Tracking castling rights
+    half_move_clock: u32, // Tracking fifty-move rule
+    game_move_counter: u32, // Game move counter
+}
+
+enum Color {
+    White,
+    Black
+}
+
+struct CastlingRights {
+    white_kingside: bool,
+    white_queenside: bool,
+    black_kingside: bool,
+    black_queenside: bool,
 }
