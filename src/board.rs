c3084207837e6756e620f5d6a5fef061877fb1f1
Sahir <sahirk2003@gmail.com>
2023-09-18 09:12:33 -0400

    created structure for Board containing bitboards

---
+++ src/board.rs
@@ -0,0 +1,14 @@
+struct Board {
+    // Piece Bitboards
+    white_pawns: u64,
+    white_knights: u64,
+    white_bishops: u64,
+    white_queen: u64,
+    white_king: u64,
+
+    black_pawns: u64,
+    black_knights: u64,
+    black_bishops: u64,
+    black_queen: u64,
+    black_king: u64,
+}
