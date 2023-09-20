7f143864bea927b4c4b4e1466c9dd1d38c8e41b1
Sahir <sahirk2003@gmail.com>
2023-09-20 11:39:27 -0400

    rook bitboard

--- src/board.rs
+++ src/board.rs
@@ -3,12 +3,14 @@ struct Board {
     white_pawns: u64,
     white_knights: u64,
     white_bishops: u64,
+    white_rooks: u64,
     white_queen: u64,
     white_king: u64,
 
     black_pawns: u64,
     black_knights: u64,
     black_bishops: u64,
+    black_rooks: u64,
     black_queen: u64,
     black_king: u64,
 
