23c24c1cae83f298f5f2125cff44d541bcb4dcb8
Sahir <sahirk2003@gmail.com>
2023-09-20 12:12:04 -0400

    Implement Board and add init_board function

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
-struct Board {
+#[derive(Debug, Clone)]
+pub struct Board {
     // Piece Bitboards
     white_pawns: u64,
     white_knights: u64,
@@ -22,11 +23,42 @@ struct Board {
     game_move_counter: u32, // Game move counter
 }
 
+impl Board {
+    pub fn init_board() -> Board {
+        Board {
+            white_pawns: 0,
+            white_knights: 0,
+            white_bishops: 0,
+            white_rooks: 0,
+            white_queen: 0,
+            white_king: 0,
+            black_pawns: 0,
+            black_knights: 0,
+            black_bishops: 0,
+            black_rooks: 0,
+            black_queen: 0,
+            black_king: 0,
+            side_to_move: Color::White,
+            en_passant_target: None,
+            castling_rights: CastlingRights { 
+                white_kingside: false, 
+                white_queenside: false, 
+                black_kingside: false, 
+                black_queenside: false 
+            },
+            half_move_clock: 0,
+            game_move_counter: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 enum Color {
     White,
     Black
 }
 
+#[derive(Debug, Clone)]
 struct CastlingRights {
     white_kingside: bool,
     white_queenside: bool,
